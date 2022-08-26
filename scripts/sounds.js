30b8f8117125166f4cbb0c947a719c15a3176a14
s-parshev <stoyan.parshev@novarto.com>
2022-08-26 12:16:32 +0300

    all sounds

--- scripts/sounds.js
+++ scripts/sounds.js
@@ -1,19 +1,33 @@
 function PlayCorrectAnswer() {
   PlayAudioFile("sounds/correct-answer.mp3");
 }
-
 function PlayFinalAnswer() {
   PlayAudioFile("sounds/final-answer.mp3");
 }
-
 function PlayPhoneAFriend() {
   PlayAudioFile("sounds/phone-a-friend.mp3");
 }
-
 function PlayWrongAnswer() {
   PlayAudioFile("sounds/phone-a-friend.mp3");
 }
-
+function PlayHundredToThousand(){
+  PlayAudioFile("sounds/soundQuestionFrom100To1000.mp3");
+}
+function PlayThousandToFiftyThousand(){
+  PlayAudioFile("sounds/soundQuestionFrom1000To50000.mp3");
+}
+function PlayFiftyThousand(){
+  PlayAudioFile("sounds/soundQuestion50000.mp3");
+}
+function PlayOneHundredThousand(){
+  PlayAudioFile("sounds/soundQuestion100000.mp3");
+}
+function PlayLetsPlaySound(){
+  PlayAudioFile("sounds/lets-play.mp3")
+}
+function PlayMainTheme(){
+  PlayAudioFile("sounds/main-theme.mp3")
+}
 function PlayAudioFile(audioName) {
   const audio = new Audio(audioName);
   audio.play();
