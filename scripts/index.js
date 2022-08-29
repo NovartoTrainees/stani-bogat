5422e24f40e5b46aaf449806e5e7ba0b3e40371a
s-parshev <stoyan.parshev@novarto.com>
2022-08-29 14:38:41 +0300

    added model handler

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,11 @@ import { askTheAudience } from "./hints.js";
 import { Visualise } from "./dom-manipulation/visualisation.js";
 import {checkTheAnswer} from "./game-logic/checkTheAnswer.js";
 import * as elements from "./dom-manipulation/elements.js";
+import {createCrowdHelpModal} from "./dom-manipulation/modal.js";
 
-
+document.getElementById('audience').addEventListener("click",()=>{
+  document.body.appendChild(createCrowdHelpModal([20,50,30,70]))
+})
 await fetchQuestions();
 
 Visualise();
