f17392a8e8783371145110657e0f9280fda57cbb
Vasil Derilov <vasil.derilov@gmail.com>
2022-08-29 14:39:08 +0300

    added modal function

---
+++ scripts/dom-manipulation/modal.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @param {[float, float, float, float]} percentages
+ * @output Modal Element
+ */
+function CreateCrowdHelpModal(percentages) {
+  const modal = document.createElement("div");
+  const canvas = document.createElement("canvas");
+
+  const context = canvas.getContext("2d");
+
+  modal.appendChild(canvas);
+  modal.classList.add("modal-back");
+
+  return modal;
+}
