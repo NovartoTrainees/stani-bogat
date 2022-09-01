5bc75befd0d46680bf6b4e713a45b372054b0c5f
Yordan Asparuhov <danny.asparuhov@gmail.com>
2022-09-01 11:55:58 +0300

    Revert "Revert "reverted""
    This reverts commit 75c05efa4b303f18689e115cd0a5b71ff25a63e5.

--- scripts/game-logic/play.js
+++ scripts/game-logic/play.js
@@ -1,11 +1,10 @@
-import { backgroundSounds, backgroundAtStart } from "./stairwayStages.js";
+
 export const playButton = document.querySelector(".play");
-playButton.addEventListener("click", startGame);
+playButton.addEventListener("click", startGame)
 
 export function startGame() {
-  backgroundAtStart();
-  const displayGame = document.querySelector(".main-container");
-  const hideStartPage = document.querySelector(".start-game-container");
-  displayGame.style.display = "block";
-  hideStartPage.style.display = "none";
+    const displayGame = document.querySelector(".main-container")
+    const hideStartPage = document.querySelector(".start-game-container")
+    displayGame.style.display = "block"
+    hideStartPage.style.display = "none"
 }
