a1a4860f17125e142818b5d9581dad45b3164f62
Yordan Asparuhov <danny.asparuhov@gmail.com>
2022-09-01 11:58:18 +0300

    Revert "Revert "Revert "reverted"""
    This reverts commit 5bc75befd0d46680bf6b4e713a45b372054b0c5f.

--- scripts/game-logic/stairwayStages.js
+++
@@ -1,9 +0,0 @@
-import { getQuestionIndex } from "../variables.js";
-export const temp = Array.from(document.querySelectorAll(".sum")).reverse();
-
-export function updateStage() {
-  temp[getQuestionIndex() - 1].classList.remove("current-sum");
-  temp[getQuestionIndex() - 1].style.opacity = 0.3;
-
-  temp[getQuestionIndex()].classList.add("current-sum");
-}
